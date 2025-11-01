a9acf5fc9c05d8b356a5cd77292d2fcab3b02019
macedera <michael.acedera@fasttrackph.com>
2025-11-01 16:51:09 +0800

    feat(): remove jwt constants

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtConstants } from './constants';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
@@ -20,7 +19,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtConstants],
+  providers: [AuthService],
   exports: [AuthService],
 })
 export class AuthModule {}
