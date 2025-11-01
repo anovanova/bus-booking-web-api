70f14b921bd9a7b73a502332a9ce8cf0d44962f6
macedera <michael.acedera@fasttrackph.com>
2025-11-01 20:16:25 +0800

    chore(): remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
